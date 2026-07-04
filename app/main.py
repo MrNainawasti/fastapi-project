3f0d892e384d2a0397db7794cb1006b77fb5ac50
Sujan Nainawasti <nainawastisujan088@gmail.com>
2026-07-04 09:06:16 +0545

    replacing strings with constants

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 from app.api.v1.api import api_router
 from app.db.database import Base, engine
 
+TITLE = "AUTH API"
+WELCOME_MESSAGE = "Welcome to Auth API"
+
 # create tables
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Auth API")
+app = FastAPI(title=TITLE)
 
 app.include_router(api_router, prefix="/api/v1")
 
 app.get("/")
 def root():
     return{
-        "message": "Welcome to Aut API"
+        "message": WELCOME_MESSAGE
     }
